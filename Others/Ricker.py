97a66ab53548642359e31e809b82a6f4077b650f
LoryPack <lorenzo.pacchiardi@stats.ox.ac.uk>
2021-02-01 14:53:12 +0100

    Update Ricker model for newer ABCpy

--- Others/Ricker.py
+++ Others/Ricker.py
@@ -1,7 +1,8 @@
-from abcpy.probabilisticmodels import ProbabilisticModel, Hyperparameter
 import numpy as np
+from abcpy.continuousmodels import ProbabilisticModel, Continuous, InputConnector
 
-class Ricker(ProbabilisticModel):
+
+class Ricker(ProbabilisticModel, Continuous):
     """Ecological model that describes the observed size of animal population over time
         described in [1].
 
@@ -15,43 +16,42 @@ class Ricker(ProbabilisticModel):
         n_timestep: int, optional
         Number of timesteps. The default value is 100.
     """
-    def __init__(self, parameters, n_timestep=100):
-        super(Ricker, self).__init__(parameters)
+
+    def __init__(self, parameters, n_timestep=100, name="Ricker"):
+        input_parameters = InputConnector.from_list(parameters)
 
         self.n_timestep = n_timestep
-        #Parameter specifying the dimension of the return values of the distribution.
+        # Parameter specifying the dimension of the return values of the distribution.
         self.dimension = n_timestep
+        super(Ricker, self).__init__(input_parameters, name)
 
-    def sample_from_distribution(self, k, rng=np.random.RandomState()):
+    def forward_simulate(self, input_values, k, rng=np.random.RandomState()):
         timeseries_array = [None] * k
         # Initialize local parameters
-        parameters = self.get_parameter_values()
-        log_r = parameters[0]
-        sigma = parameters[1]
-        phi = parameters[2]
+        log_r = input_values[0]
+        sigma = input_values[1]
+        phi = input_values[2]
 
-        for k in range(0, k):
+        for i in range(0, k):
             # Initialize the time-series
-            timeseries_obs_size = np.zeros(shape=(self.n_timestep), dtype=np.float)
-            timeseries_true_size = np.ones(shape=(self.n_timestep), dtype=np.float)
+            timeseries_obs_size = np.zeros(shape=self.n_timestep, dtype=np.float)
+            timeseries_true_size = np.ones(shape=self.n_timestep, dtype=np.float)
             for ind in range(1, self.n_timestep - 1):
                 timeseries_true_size[ind] = np.exp(log_r + np.log(timeseries_true_size[ind - 1]) - timeseries_true_size[
                     ind - 1] + sigma * rng.normal(0, 1))
                 timeseries_obs_size[ind] = rng.poisson(phi * timeseries_true_size[ind])
-            timeseries_array[k] = timeseries_obs_size
+            timeseries_array[i] = timeseries_obs_size
 
         # return an array of objects of type Timeseries
-        return [True, np.array(timeseries_array)]
+        return timeseries_array
 
-    def _check_parameters_before_sampling(self, parameters):
+    def _check_input(self, input_values):
+        """
+        """
+        if len(input_values) != 3:
+            return False
+        # shold also check range for the different parameters
         return True
 
-    def _check_parameters_fixed(self, parameters):
+    def _check_output(self, values):
         return True
-
-    def _check_parameters_at_initialization(self, parameters):
-        if(len(parameters)!=3):
-            raise ValueError('The provided parameters do not have length 3.')
-
-    def pdf(self, x):
-        raise NotImplementedError
